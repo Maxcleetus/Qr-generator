d646e8d5e6e656be9966dc904cafdd8d401a4355
MAX <maxcleetus@gmail.com>
2024-11-17 18:43:51 +0530

    fifth version

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ let geturl = document.getElementById("geturl")
 let display = document.getElementById("display")
 let qrimage = document.getElementById("qrimage")
 
+
+
 function clicking(){
 if(geturl.value.length > 0){
-        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+geturl.value
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&color=4a3e343&bgcolor=a5a091&data="+geturl.value
+}else{
+    geturl.classList.add("error")
+    setTimeout(()=>{
+        geturl.classList.remove("error")
+    },1000)
+}
+}
+let d = document.querySelector("bt")
+function down(){
+    if(geturl.value.length > 0){
+        d.href = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&color=4a3e343&bgcolor=a5a091&data="+geturl.value
+        console.log(d.href)
 }else{
     geturl.classList.add("error")
     setTimeout(()=>{
